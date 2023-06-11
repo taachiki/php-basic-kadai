9e53f2ccd5f5fe8751ccff0bf0a2ab28e0f89cd6
taachiki <123560767+taachiki@users.noreply.github.com>
2023-06-11 19:00:34 +0900

    Add files via upload

--- class.php
+++ class.php
@@ -1,4 +1,12 @@
-<p>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>PHPの勉強</title>
+</head>
+
+<body>
 <?php
 
 class Food{
@@ -10,17 +18,16 @@ public function __construct(string $name, int $price){
   $this->price = $price;
 }
 
+public function show_price(){
+  echo $this->price . '<br>';
+}
+
 }
 
 $food = new Food('potato', 250);
 
 print_r($food);
-
-
-?>
-</p>
-<p>
-<?php
+echo '<br>';
 
 class Animal{
   private $name;
@@ -32,10 +39,19 @@ class Animal{
     $this->height = $height;
     $this->weight = $weight;
   }
+
+  public function show_height(){
+    echo $this->height;
+  }
 }
 
 $animal = new Animal('dog', 60, 5000);
-
 print_r($animal);
+echo '<br>';
+
+$food->show_price();
+$animal->show_height();
 ?>
-</p>
+</body>
+
+</html>
